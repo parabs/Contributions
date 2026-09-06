98c234110be5f6687b5e19d227285cc883cf38c1
parabs <48829808+parabs@users.noreply.github.com>
2026-09-07 01:15:36 +0530

    alert for udpate record

--- src/App.tsx
+++ src/App.tsx
@@ -332,7 +332,8 @@ const [trustConfig, setTrustConfig] = useState<TrustConfig>(() => {
       const updatedRecord: DonationRecord = result.donation;
 
       console.log('VERIFIED DONATION FROM SHEET:', updatedRecord);
-      
+      alert(JSON.stringify(updatedRecord, null, 2));
+
       setDonations(prev => {
         const updated = prev.some(
           d => d.donationId === updatedRecord.donationId
