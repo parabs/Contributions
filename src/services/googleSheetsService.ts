b01e075c6bdd2a1ab163e320e8488d1aab223d00
parabs <48829808+parabs@users.noreply.github.com>
2026-09-17 10:06:17 +0530

    alert add volunteer

--- src/services/googleSheetsService.ts
+++ src/services/googleSheetsService.ts
@@ -1184,6 +1184,8 @@ export async function addVolunteer(
 
     const responseText = await response.text();
 
+    alert('ADD VOLUNTEER RAW RESPONSE:\n\n' + responseText);
+    
     try {
       const result = JSON.parse(responseText);
 
